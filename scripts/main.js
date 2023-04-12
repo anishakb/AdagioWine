2da2a5b6539effa468e9ff7467ae61ae4892661a
anishab11 <abhalla11@my.bcit.ca>
2023-04-11 20:00:10 -0700

    Remaining issues on github fixed
    Including feature requests for Go To Top button and addition of Title Tags

--- scripts/main.js
+++ scripts/main.js
@@ -125,7 +125,32 @@ jQuery(function($) {
     });
     });
 
+/*
+|------------------------------------------
+|  Go to Top Button
+|------------------------------------------
+*/
 
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 
